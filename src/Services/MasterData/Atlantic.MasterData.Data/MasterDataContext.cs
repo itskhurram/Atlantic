89c8f823877f00b4de7b6e7c6c0322ea30957ce6
itskhurram <itskhurrams@outlook.com>
2021-05-08 17:33:28 +0500

    added dbContext

---
+++ src/Services/MasterData/Atlantic.MasterData.Data/MasterDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Atlantic.MasterData.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atlantic.MasterData.Data {
+    public class MasterDataContext : DbContext {
+        public MasterDataContext(DbContextOptions options) : base(options) {
+
+        }
+        public DbSet<Countries> Countries { get; set; }
+        public DbSet<States> States { get; set; }
+        public DbSet<Cities> Cities { get; set; }
+        public DbSet<BusinessServices> BusinessServices { get; set; }
+    }
+}
