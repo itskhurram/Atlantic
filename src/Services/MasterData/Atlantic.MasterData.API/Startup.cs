ed37b879e86af38db92020f5de306d0bb6eaf91b
itskhurram <itskhurrams@outlook.com>
2021-05-08 19:28:28 +0500

    Migration Ready Need to Update Models

--- src/Services/MasterData/Atlantic.MasterData.API/Startup.cs
+++ src/Services/MasterData/Atlantic.MasterData.API/Startup.cs
@@ -1,8 +1,14 @@
+using Atlantic.MasterData.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +16,38 @@ using System.Threading.Tasks;
 
 namespace Atlantic.MasterData.API {
     public class Startup {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration config) {
+            _configuration = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<MasterDataContext>(options => {
+                options.UseSqlite(_configuration.GetConnectionString("MasterDataDbConnection"));
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Atlantic.MasterData.API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Atlantic.MasterData.API v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
