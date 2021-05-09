fab3d09dcc914d9e1c652a6576f6ce61a770978c
itskhurram <itskhurrams@outlook.com>
2021-05-09 20:18:16 +0500

    WorldInformationRepository DONE

---
+++ src/Services/MasterData/Atlantic.MasterData.Domain/Interfaces/IWorldInformationRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Atlantic.MasterData.Domain.Models;
+namespace Atlantic.MasterData.Domain.Interfaces {
+    public interface IWorldInformationRepository {
+        Task<IEnumerable<Countries>> GetCountries();
+        Task<IEnumerable<States>> GetStatesByCountryId(int CountryId);
+        Task<IEnumerable<Cities>> GetCitiesByCountryStateId(int StateId,int CountryId);
+    }
+}
